6552d9f11e77e72df61fe8ece3183412705a36af
UmarBis <bis.umar@icloud.com>
2025-11-26 00:05:47 +0400

    Update task1_86-32.asm

--- task1_86-32.asm
+++ task1_86-32.asm
@@ -2,79 +2,82 @@ section .data
     prompt db "Enter x value: ", 0
     format_in db "%f", 0
     format_out db "exp(%f) = %f", 10, 0
-    one dd 1.0
-    two dd 2.0
-    six dd 6.0
-    twentyfour dd 24.0
-
-section .bss
-    x_val resd 1
-    temp resd 1
 
 section .text
 global main
-extern printf, scanf
+extern printf, scanf, malloc, free
 
 main:
+    ; СТАНДАРТНЫЙ ПРОЛОГ
     push ebp
     mov ebp, esp
+    
+    ; ВЫДЕЛЕНИЕ ПАМЯТИ ДИНАМИЧЕСКИ
+    push 4                   ; sizeof(float)
+    call malloc
+    add esp, 4
+    mov ebx, eax            ; Сохраняем указатель
     
     ; ВВОД ЗНАЧЕНИЯ X
     push prompt
     call printf
     add esp, 4
     
-    push x_val
+    push ebx                ; указатель на выделенную память
     push format_in
     call scanf
     add esp, 8
     
     ; ВЫЧИСЛЕНИЕ EXP(X) ЧЕРЕЗ FPU
-    fld dword [x_val]       ; st0 = x
+    fld dword [ebx]         ; st0 = x
     
-    ; Ряд Тейлора: exp(x) ≈ 1 + x + x²/2! + x³/3! + x⁴/4!
+    ; Ряд Тейлора через FPU
     fld1                    ; st0 = 1.0, st1 = x
+    fadd st0, st1           ; 1 + x
     
-    ; 1 + x
-    fadd st0, st1           ; st0 = 1 + x
-    
-    ; + x²/2!
-    fld st1                 ; st0 = x, st1 = 1+x, st2 = x
-    fmul st0, st0           ; st0 = x²
-    fdiv dword [two]        ; st0 = x²/2
-    faddp                   ; st0 = 1 + x + x²/2
-    
-    ; + x³/3!
     fld st1                 ; st0 = x, st1 = результат, st2 = x
-    fmul st0, st0           ; st0 = x²
-    fmul st0, st2           ; st0 = x³
-    fdiv dword [six]        ; st0 = x³/6
-    faddp                   ; st0 = 1 + x + x²/2 + x³/6
+    fmul st0, st0           ; x²
+    fild dword [two]        ; st0 = 2.0
+    fdivp                   ; x²/2
+    faddp                   ; + x²/2
     
-    ; + x⁴/4!
-    fld st1                 ; st0 = x, st1 = результат, st2 = x
-    fmul st0, st0           ; st0 = x²
-    fmul st0, st0           ; st0 = x⁴
-    fdiv dword [twentyfour] ; st0 = x⁴/24
-    faddp                   ; st0 = окончательный результат
+    fld st1                 ; st0 = x
+    fmul st0, st0           ; x²
+    fmul st0, st2           ; x³
+    fild dword [six]        ; st0 = 6.0
+    fdivp                   ; x³/6
+    faddp                   ; + x³/6
     
-    ; Сохраняем результат
-    fstp dword [temp]
+    fld st1                 ; st0 = x
+    fmul st0, st0           ; x²
+    fmul st0, st0           ; x⁴
+    fild dword [twentyfour] ; st0 = 24.0
+    fdivp                   ; x⁴/24
+    faddp                   ; + x⁴/24
     
     ; ВЫВОД РЕЗУЛЬТАТА
     sub esp, 8
-    fld dword [temp]        ; загружаем результат
-    fstp qword [esp]        ; конвертируем в double и кладем на стек
+    fst qword [esp]         ; результат как double
     
     sub esp, 8
-    fld dword [x_val]       ; загружаем x
-    fstp qword [esp]        ; конвертируем в double и кладем на стек
+    fld dword [ebx]
+    fstp qword [esp]        ; x_val как double
     
     push format_out
     call printf
     add esp, 20
     
-    ; Завершение программы
+    ; ОСВОБОЖДЕНИЕ ПАМЯТИ
+    push ebx
+    call free
+    add esp, 4
+    
+    ; СТАНДАРТНЫЙ ЭПИЛОГ
     pop ebp
     xor eax, eax
     ret
+
+; Константы в коде (не в .data!)
+two: dd 2
+six: dd 6  
+twentyfour: dd 24
